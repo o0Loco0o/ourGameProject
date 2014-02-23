b4e7c5a5bb1ecfdc7f5ce964669d50740ecbd19d
o0Loco0o <bryon.dillon@gmail.com>
2014-02-23 11:52:25 -0800

    Initial Commit

--- Assets/Scripts/Player/playerAnimationControl.cs
+++ Assets/Scripts/Player/playerAnimationControl.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class playerControl : MonoBehaviour {
+public class playerAnimationControl : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
